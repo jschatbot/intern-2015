e3d619f860a0faf5fc2915caf090ba684a14f9b7
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-12 17:40:31 +0900

    [modify] use json(dict, hash) style

--- example.py
+++ example.py
@@ -10,9 +10,12 @@ class API:
     def sentences(self, sentences):
         url = '/jmat/sentence'
         query = {'query': sentences}
-        return requests.get(self.url + url, params=query, auth=self.auth, verify=False)
+        return requests.get(self.url + url, params=query, auth=self.auth, verify=False).json()
+
 
 api = API('https://52.68.75.108', 'secret', 'js2015cps')
+
 s = '日本語文字列を文単位で分割する。複数文を渡すと、文ごとに区切ってくれる。'
-print api.sentences(s).text
+for sentence in  api.sentences(s)['sentences']:
+    print sentence
 
