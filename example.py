33ef7c3b12170740013b50b15ac75deb743192a7
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-12 18:01:22 +0900

    [add] twitter api

--- example.py
+++ example.py
@@ -12,6 +12,23 @@ class API:
         query = {'query': sentences}
         return requests.get(self.url + url, params=query, auth=self.auth, verify=False).json()
 
+    def tweet(self, message):
+        url = '/tweet/simple'
+        name = 'js_devbot04'
+        query = {'bot_name': name, 'message': message}
+        return requests.post(self.url + url, params=query, auth=self.auth, verify=False).json()
+
+    def get_reply(self):
+        url = '/tweet/get_reply'
+        name = 'js_devbot04'
+        query = {'bot_name': name}
+        return requests.get(self.url + url, params=query, auth=self.auth, verify=False).json()
+
+    def search_tweets(self, query, limit=10):
+        url = '/search/tweet'
+        query = {'query': query, 'limit': limit}
+        return requests.get(self.url + url, params=query, auth=self.auth, verify=False).json()
+
 
 api = API('https://52.68.75.108', 'secret', 'js2015cps')
 
@@ -19,3 +36,9 @@ s = '日本語文字列を文単位で分割する。複数文を渡すと、文
 for sentence in  api.sentences(s)['sentences']:
     print sentence
 
+for text in api.search_tweets('検索')['texts']:
+    print text
+
+# エラーが出る。。。
+#print api.tweet('ツイートテスト! ')
+#print api.get_reply()
