79593f1b40e627fcc5ea6ac5463b3e83ea99aef3
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-12 17:35:47 +0900

    [modify] use api class

--- example.py
+++ example.py
@@ -2,15 +2,17 @@
 
 import requests
 
-auth = ('secret', 'js2015cps')
-url = 'https://52.68.75.108'
+class API:
+    def __init__(self, url, usr, passwd):
+        self.url = url
+        self.auth = (usr, passwd)
 
-query = {'query': '日本語文字列を文単位で分割する。複数文を渡すと、文ごとに区切ってくれる。'}
-res = requests.get(url + '/jmat/sentence', auth=auth, params=query, verify=False)
-print res.text
+    def sentences(self, sentences):
+        url = '/jmat/sentence'
+        query = {'query': sentences}
+        return requests.get(self.url + url, params=query, auth=self.auth, verify=False)
 
-
-query = {'query': '日本語文字列を形態素（単語）単位で分割する。複数文を渡すと、文ごとに区切ってくれる。'}
-res = requests.get(url + '/jmat/morph', auth=auth, params=query, verify=False)
-print res.text
+api = API('https://52.68.75.108', 'secret', 'js2015cps')
+s = '日本語文字列を文単位で分割する。複数文を渡すと、文ごとに区切ってくれる。'
+print api.sentences(s).text
 
