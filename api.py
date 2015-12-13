abe5421e6c5aff4701f447b93d318aa70082f59c
ryosukee <tmcit.miyazaki@gmail.com>
2015-12-13 10:17:32 +0900

    [add] initial commit

---
+++ api.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+class API:
+    def __init__(self, url, usr, passwd):
+        self.url = url
+        self.auth = (usr, passwd)
+
+    def __get(self, url, query):
+        return requests.get(url, params=query, auth=self.auth, verify=False).json()
+    
+    def __post(self, url, query):
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        return requests.post(url, data=json.dumps(query), auth=self.auth, verify=False, headers=headers).json()
+
+    def sentences(self, sentences):
+        url = self.url + '/jmat/sentence'
+        query = {'query': sentences}
+        return self.__get(url, query)
+    
+    def morph(self, sentence):
+        url = self.url + '/jmat/morph'
+        query = {'query': sentence}
+        return self.__get(url, query)
+
+    def chunk(self, sentence):
+        url = self.url + '/jmat/chunk'
+        query = {'query': sentence}
+        return self.__get(url, query)
+
+    def synonym(self, sentence):
+        url = self.url + '/jmat/synonym'
+        query = {'query': sentence}
+        return self.__get(url, query)
+
+    def tweet(self, message):
+        url = self.url + '/tweet/simple'
+        name = 'js_devbot04'
+        query = {'bot_name': name, 'message': message}
+        return self.__post(url, query)
+
+    def get_reply(self):
+        url = self.url + '/tweet/get_reply'
+        name = 'js_devbot04'
+        query = {'bot_name': name}
+        return self.__get(url, query)
+
+    def search_tweets(self, query, limit=10):
+        url = self.url + '/search/tweet'
+        query = {'query': query, 'limit': limit}
+        return self.__get(url, query)
+    
+    def search_reply(self, query, limit=10):
+        url = self.url + '/search/reply'
+        query = {'query': query, 'limit': limit}
+        return self.__get(url, query)
+
+    def markov_chain(self, seed):
+        url = self.url + '/tk/markov'
+        query = {'surface': seed['norm_surface'], 'pos': seed['pos']}
+        return self.__get(url, query)
+    
+    def rewrite_morph(self, file_name, morphs):
+        url = self.url + '/tk/rewrite'
+        query = {'rule': file_name, 'morphs': morphs}
+        return self.__post(url, query)
+    
+    def trigger(self, filename ,morphs):
+        url = self.url + '/tk/trigger'
+        quary = {'rule': filename, 'morphs': morphs }
+        return self.__post(url, query)
+    
+    def send_reply(self, mention_id, user_name, message):
+        url = self.url + '/tweet/send_reply'
+        name = 'js_devbot04'
+        query = {'bot_name': name, 'replies': [{ 'mention_id': mention_id, 'user_name': user_name, 'message': message } ] }
+        return self.__post(url, query)
